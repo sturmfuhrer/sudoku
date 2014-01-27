3fd7533b7082f0826082f6e38f7693ddb7e2046c
Sorcer26 <andriy-valentuk@ukr.net>
2014-01-27 14:34:34 +0200

    Interface created

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Sudoku
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MaindataGridView = new System.Windows.Forms.DataGridView();
+            this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.новаГраToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.складністьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вихідToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.MaindataGridView)).BeginInit();
+            this.MainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MaindataGridView
+            // 
+            this.MaindataGridView.AllowUserToAddRows = false;
+            this.MaindataGridView.AllowUserToDeleteRows = false;
+            this.MaindataGridView.AllowUserToResizeColumns = false;
+            this.MaindataGridView.AllowUserToResizeRows = false;
+            this.MaindataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MaindataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MaindataGridView.ColumnHeadersVisible = false;
+            this.MaindataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MaindataGridView.GridColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.MaindataGridView.Location = new System.Drawing.Point(0, 29);
+            this.MaindataGridView.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.MaindataGridView.Name = "MaindataGridView";
+            this.MaindataGridView.RowHeadersVisible = false;
+            this.MaindataGridView.Size = new System.Drawing.Size(316, 317);
+            this.MaindataGridView.TabIndex = 0;
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новаГраToolStripMenuItem,
+            this.складністьToolStripMenuItem,
+            this.вихідToolStripMenuItem});
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Padding = new System.Windows.Forms.Padding(18, 5, 0, 5);
+            this.MainMenu.Size = new System.Drawing.Size(316, 29);
+            this.MainMenu.TabIndex = 1;
+            this.MainMenu.Text = "menuStrip1";
+            // 
+            // новаГраToolStripMenuItem
+            // 
+            this.новаГраToolStripMenuItem.Name = "новаГраToolStripMenuItem";
+            this.новаГраToolStripMenuItem.Size = new System.Drawing.Size(71, 19);
+            this.новаГраToolStripMenuItem.Text = "Нова гра ";
+            // 
+            // складністьToolStripMenuItem
+            // 
+            this.складністьToolStripMenuItem.Name = "складністьToolStripMenuItem";
+            this.складністьToolStripMenuItem.Size = new System.Drawing.Size(79, 19);
+            this.складністьToolStripMenuItem.Text = "Складність";
+            // 
+            // вихідToolStripMenuItem
+            // 
+            this.вихідToolStripMenuItem.Name = "вихідToolStripMenuItem";
+            this.вихідToolStripMenuItem.Size = new System.Drawing.Size(47, 19);
+            this.вихідToolStripMenuItem.Text = "Вихід";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 346);
+            this.Controls.Add(this.MaindataGridView);
+            this.Controls.Add(this.MainMenu);
+            this.Font = new System.Drawing.Font("Times New Roman", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainMenuStrip = this.MainMenu;
+            this.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Судоку";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MaindataGridView)).EndInit();
+            this.MainMenu.ResumeLayout(false);
+            this.MainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView MaindataGridView;
+        private System.Windows.Forms.MenuStrip MainMenu;
+        private System.Windows.Forms.ToolStripMenuItem новаГраToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem складністьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вихідToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            MaindataGridView.ColumnCount = 9;
+            MaindataGridView.RowCount = 9;
+            for (int i = 0; i < 9; i++)
+            {
+                MaindataGridView.Rows[i].Height = 35;
+                MaindataGridView.Columns[i].Width = 35;
+            }
+            MaindataGridView.Columns[2].DividerWidth = 3;
+            MaindataGridView.Columns[5].DividerWidth = 3;
+            MaindataGridView.Rows[2].DividerHeight = 3;
+            MaindataGridView.Rows[5].DividerHeight = 3;
+        }
+
+    }
+}
