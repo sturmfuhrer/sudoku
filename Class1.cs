247b718470fa679dfccdb8149a3f5417fd487960
starbax <vasya28@gmail.com>
2014-01-28 13:05:57 +0200

    verf1.0

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Verification
+{
+	public Verification()
+	{
+	} 
+
+    int check(int a,int b){
+         int[,] m2= new int[9,9]; int r=0;
+         for(int i=0;i<9;i++)
+          for(int j=0;j<9;j++)
+           m2[i][j]=mas[r++];
+         for (int i=0;i<3;i++)
+          for (int j=0;j<3;j++)
+          {if ((3*(a/3)+i==a)&&(3*(b/3)+j==b)) continue;
+         if (m2[3*(a/3)+i][3*(b/3)+j]==0) continue;
+         if (m2[3*(a/3)+i][3*(b/3)+j]==m2[a][b]) return 1;
+    };
+    }
+
+}
