e1635ba45c958813865cbf11d7730cb84d0353e7
sturmfuhrer <nyzhin@mail.ru>
2014-01-29 08:59:24 +0200

    veref1.1

--- Class1.cs
+++ Class1.cs
@@ -18,5 +18,20 @@ public class Verification
          if (m2[3*(a/3)+i][3*(b/3)+j]==m2[a][b]) return 1;
     };
     }
+	
+	int check_lines(int a,int b){
+	int[,] m2= new int[9,9]; int r=0;
+	 for(int i=0;i<9;i++)
+	  for(int j=0;j<9;j++)
+	   m2[i][j]=mas[r++];
+	 for(int i=0;i<9;i++)
+	 {if (i==b) continue;
+	 if (m2[a][i]==0) continue;
+	 if (m2[a][i]==m2[a][b]) return 1;};
+	 for(int i=0;i<9;i++)
+	 {if (i==a) continue;
+	 if (m2[i][b]==0) continue;
+	 if (m2[i][b]==m2[a][b]) return 2;};
+	}
 
 }
